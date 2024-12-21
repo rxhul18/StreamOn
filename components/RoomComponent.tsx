4020844b4c333ee8981f9e091478ac9a72996c84
rxhul18 <rahulshah0728@gmail.com>
2024-12-21 20:16:22 +0530

    EsLint

--- components/RoomComponent.tsx
+++ components/RoomComponent.tsx
@@ -1,9 +1,6 @@
 'use client';
 
-import useUser from '@/app/hooks/useUser';
-
 import dynamic from "next/dynamic";
-import { useSearchParams } from 'next/navigation';
 
 const LiveStream = dynamic(() => import("@/components/LiveStream"),{
   ssr: false
